9d5c8d436a8a659b7b796df26882e56cf2f6d3f8
Zach Rose <zach@zachrose.biz>
2016-11-13 01:40:46 -0800

    Dynamically style color picker and button

--- source.js
+++ source.js
@@ -1,4 +1,5 @@
 var Color = require('color');
+var tinygradient = require('tinygradient');
 
 var getColorInputs = function(doc){
     return ['hue', 'lightness', 'saturation'].map(function(dimension){
@@ -23,9 +24,43 @@ var setShirtColor = function(doc){
     }
 }
 
+var setColorPickerColor = function(doc){
+    var setPickerBackground = function(dimension){
+        return function(color){
+            var _color = Color(color);
+            doc.getElementById(dimension).style.background = tinygradient([
+                {pos: 0.0, color: _color[dimension](000).hexString()},
+                {pos: 0.5, color: _color[dimension](050).hexString()},
+                {pos: 1.0, color: _color[dimension](100).hexString()}
+            ]).css();
+        }
+    };
+    return function(color){
+        setPickerBackground('saturation')(color);
+        setPickerBackground('lightness')(color);
+    }
+}
+
+var setButton = function(doc){
+    return function(color, lightOrDark){
+        var hex = Color(color).hexString();
+        var text = `VOTE FOR ${hex}`;
+        var element = doc.getElementById('vote');
+        element.innerText = text;
+        element.style.backgroundColor = hex;
+        element.classList.remove('light');
+        element.classList.remove('dark');
+        element.classList.add(lightOrDark);
+    }
+}
+
 setInterval(function(){
     var color = getColorInputs(document);
-    console.log(color);
+    setColorPickerColor(document)(color);
+    setButton(document)(
+        color,
+        Color(color).dark() ? 'light' : 'dark'
+    )
     setShirtColor(document)(
         Color(color).hslString(),
         Color(color).dark() ? 'light' : 'dark'
